a15ddeb10ae2b2b5812ef4c39069a650da8e30c8
ZacCarman <zachary.carman@ontariotechu.net>
2019-11-13 08:33:55 -0500

    Base JavaScript

---
+++ script.js
@@ -0,0 +1,75 @@
+var Clementines=0;
+var Apples=0;
+var Bananas=0;
+var Broccoli=0;
+var Eggplant=0;
+var Lettuce=0;
+var Cake=0;
+var Cheese=0;
+var Chocolate=0;
+var Brownies=0;
+var Fritter=0;
+var Donuts=0;
+var Steak=0;
+var Chicken=0;
+var Pork=0;
+var Lobster=0;
+var Salmon=0;
+var Duck=0;
+
+
+
+function ClementinesINC(){
+  Clementines++;
+}
+function ApplesINC(){
+  Apples++;
+}
+function BananasINC(){
+  Bananas++;
+}
+function BroccoliINC(){
+  Broccoli++;
+}
+function EggplantINC(){
+  Eggplant++;
+}
+function LettuceINC(){
+  Lettuce++;
+}
+function CakeINC(){
+  Cake++;
+}
+function CheeseINC(){
+  Cheese++;
+}
+function ChocolateINC(){
+  Chocolate++;
+}
+function BrowniesINC(){
+  Brownies++;
+}
+function FritterINC(){
+  Fritter++;
+}
+function DonutsINC(){
+  Donuts++;
+}
+function SteakINC(){
+  Steak++;
+}
+function ChickenINC(){
+  Chicken++;
+}
+function PorkINC(){
+  Pork++;
+}
+function LobsterINC(){
+  Lobster++;
+}
+function DuckINC(){
+  Duck++;
+}
+function SalmonINC(){
+  Salmon++;
+}
