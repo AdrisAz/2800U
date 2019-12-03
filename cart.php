dd6760739ca2e3719651246266597d6e3a6cf038
AdrisAz <AdrisAz@users.noreply.github.com>
2019-12-02 21:23:16 -0500

    card php

--- cart.php
+++ cart.php
@@ -1,17 +1,17 @@
 <?php
 
-include_once("ShoppingCart.class.php");
 session_start();
+$connect=mysqli_connect("localhost","username","password","dbname" )
+$query="SELECT * FROM tbl_product ORDER BY id ASC";
+$result=mysqli_query($connect,$query);
+if(mysqli_num_rows($result)>0)
+{
+while($row= mysqli_fetch_array($result))
+{
 
-if(isset($_SESSION['user'])) {
-
-  $_SESSION["Cart"]=new ShoppingCart();
 }
-$cart=$_SESSION["Cart"];
-else {
-
 }
+
 
 
 ?>
-[]
