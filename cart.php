3711d1541cfa4b8ed2d0928499c38a04dcf3ccc9
AdrisAz <AdrisAz@users.noreply.github.com>
2019-12-03 14:54:59 -0500

    many changes lol

--- cart.php
+++ cart.php
@@ -1,10 +1,15 @@
 <?php
 
 session_start();
-$connect=mysqli_connect("localhost","password", "dbname");
+$connect=mysqli_connect("localhost","UserName","password", "dbname");
 
 $query="SELECT * FROM tbl_product ORDER BY id ASC";
 $result=mysqli_query($connect,$query);
+if(mysqli_num_rows($result)>0)
+{
+  while($row=mysqli_fetch_array($result))
+}
+
 
 if(isset($_SESSION['user'])) {
 
